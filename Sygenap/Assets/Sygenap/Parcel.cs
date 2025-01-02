4755efcc610825877febf30282048b1297527687
thomasgainant <thomasgainant@gmail.com>
2025-01-02 23:04:07 +0100

    saving system

--- Sygenap/Assets/Sygenap/Parcel.cs
+++ Sygenap/Assets/Sygenap/Parcel.cs
@@ -1,9 +1,18 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace Sygenap
-{    public class Parcel : MonoBehaviour
+{
+    public class Parcel : MonoBehaviour
     {
         public Sygenap root;
+
+        private int _x;
+        public int x { get { return _x; } }
+
+        private int _y;
+        public int y { get { return _y; } }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -11,10 +20,61 @@ namespace Sygenap
 
         }
 
+        public void init(Sygenap root, int x, int y)
+        {
+            this.root = root;
+            this._x = x;
+            this._y = y;
+        }
+
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        public void save()
+        {
+            string destination = Application.persistentDataPath + "/" + this.root.gameName + "/" + this._x + "-" + this._y;
+            FileStream file;
+
+            if (File.Exists(destination)) file = File.OpenWrite(destination);
+            else file = File.Create(destination);
+
+            ParcelData data = new ParcelData(this._x, this._y);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(file, data);
+            file.Close();
+        }
+
+        public void load()
+        {
+            string destination = Application.persistentDataPath + "/" + this.root.gameName + "/" + this._x + "-" + this._y;
+            FileStream file;
+
+            if (File.Exists(destination)) file = File.OpenRead(destination);
+            else
+            {
+                Debug.LogError("File not found");
+                return;
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+            ParcelData data = (ParcelData)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+
+    [System.Serializable]
+    public class ParcelData
+    {
+        public int x;
+        public int y;
+
+        public ParcelData(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
--- Sygenap/Assets/Sygenap/Sygenap.cs
+++ Sygenap/Assets/Sygenap/Sygenap.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace Sygenap {
@@ -12,16 +14,95 @@ namespace Sygenap {
             }
         }
 
+        private bool randomSeed = true;
+
+        public string gameName;
+
+        private POV _pov;
+        public POV pov
+        {
+            get { return _pov; }
+        }
+        public GameObject povPrefab;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            if (File.Exists(this.getSaveFileURI()))
+                this.loadLevel();
+            else
+            {
+                if (this.randomSeed)
+                    this._seed = Random.Range(int.MinValue, int.MaxValue);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
+        }
+
+        public string getSaveFileURI()
+        {
+            return Application.persistentDataPath + "/" + this.gameName + "/." + this._seed;
+        }
+
+        public void saveLevel()
+        {
+            string destination = this.getSaveFileURI();
+            FileStream file;
+
+            if (File.Exists(destination)) file = File.OpenWrite(destination);
+            else file = File.Create(destination);
+
+            SygenapData data = new SygenapData(this._seed, this._pov.transform.position, this._pov.transform.localRotation);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(file, data);
+            file.Close();
+        }
+
+        private void loadLevel()
+        {
+            string destination = this.getSaveFileURI();
+            FileStream file;
+
+            if (File.Exists(destination)) file = File.OpenRead(destination);
+            else
+            {
+                Debug.LogError("File not found");
+                return;
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+            SygenapData data = (SygenapData)bf.Deserialize(file);
+            file.Close();
+
+            this.spawnPOV(data.povPosition, data.povRotation);
+        }
+
+        private void spawnPOV(Vector3 position, Quaternion rotation)
+        {
+            GameObject povObj = Instantiate(this.povPrefab);
+            povObj.transform.position = position;
+            povObj.transform.localRotation = rotation;
+
+            POV pov = povObj.AddComponent<POV>();
+        }
+    }
+
+    [System.Serializable]
+    public class SygenapData{
+        public int seed;
+        public Vector3 povPosition;
+        public Quaternion povRotation;
+
+        public SygenapData(int seed, Vector3 povPosition, Quaternion povRotation)
+        {
+            this.seed = seed;
+            this.povPosition = povPosition;
+            this.povRotation = povRotation;
         }
     }
 }
